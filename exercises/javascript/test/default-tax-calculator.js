392bdce54e023b93ee8b817aedd10b7b6a0f5a08
luke-chant <luke.chant@autotrader.co.uk>
2021-05-17 15:27:59 +0100

    add passing diesel code

--- exercises/javascript/test/default-tax-calculator.js
+++ exercises/javascript/test/default-tax-calculator.js
@@ -37,6 +37,36 @@ let DefaultTaxCalculator = class DefaultTaxCalculator extends TaxCalculator {
       }
       
     }
+    if (vehicle.fuelType === "Diesel") {
+        if (vehicle.co2Emissions === 0) {
+          taxAmount = 0;
+        } else if (vehicle.co2Emissions > 0 && vehicle.co2Emissions <= 50) {
+          taxAmount = 25;
+        } else if (vehicle.co2Emissions > 50 && vehicle.co2Emissions <= 75) {
+          taxAmount = 105;
+        } else if (vehicle.co2Emissions > 75 && vehicle.co2Emissions <= 90) {
+          taxAmount = 125;
+        } else if (vehicle.co2Emissions > 90 && vehicle.co2Emissions <= 100) {
+          taxAmount = 145;
+        } else if (vehicle.co2Emissions > 100 && vehicle.co2Emissions <= 110) {
+          taxAmount = 165;
+        } else if (vehicle.co2Emissions > 110 && vehicle.co2Emissions <= 130) {
+          taxAmount = 205;
+        } else if (vehicle.co2Emissions > 130 && vehicle.co2Emissions <= 150) {
+          taxAmount = 515;
+        } else if (vehicle.co2Emissions > 150 && vehicle.co2Emissions <= 170) {
+          taxAmount = 830;
+        } else if (vehicle.co2Emissions > 170 && vehicle.co2Emissions <= 190) {
+          taxAmount = 1240;
+        } else if (vehicle.co2Emissions > 190 && vehicle.co2Emissions <= 225) {
+          taxAmount = 1760;
+        } else if (vehicle.co2Emissions > 225 && vehicle.co2Emissions <= 255) {
+          taxAmount = 2070;
+        } else if (vehicle.co2Emissions > 225) {
+          taxAmount = 2070;
+        }
+        
+      }
     return taxAmount;
   }
 };
